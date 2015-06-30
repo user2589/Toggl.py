48f2d3f39798c854765da1f838f463b3164fb67f
Marat <valiev.m@gmail.com>
2015-06-30 09:42:26 +0300

    PEP8 fixes

--- overachievers.py
+++ overachievers.py
@@ -11,18 +11,21 @@ import report
 
 from collections import defaultdict
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='List time records longer than N hours (10 by default) in CSV format.')
+        description="List time records longer than N hours "
+                    "(10 by default) in CSV format.")
     parser.add_argument('-d', '--date', help='system date override, YYYY-MM-DD')
-    parser.add_argument('-n', '--threshold', type=int, default=10, help='time record threshold in hours')
-    parser.add_argument('-v', '--verbose', help="Verbose mode, 5 - extra verbose, "
-                        "1 - mute, default: 3", default=3)
+    parser.add_argument('-n', '--threshold', type=int, default=10,
+                        help='time record threshold in hours')
+    parser.add_argument('-v', '--verbose',
+                        help="Verbose mode, 5 - extra verbose, "
+                             "1 - mute, default: 3", default=3)
     args = parser.parse_args()
 
     # configure verboseness
     try:
-        verboseness = max(1, 5 - int(args.verbose)*1) * 10
+        verboseness = max(1, 5 - int(args.verbose) * 1) * 10
     except ValueError:
         verboseness = 30
     logging.basicConfig(level=verboseness)
@@ -30,7 +33,8 @@ if __name__ =='__main__':
     _date_format = report._date_format
     if args.date is None:
         date = datetime.datetime.now()
-        logging.debug("No date specified, using system date: %s", date.strftime(_date_format))
+        logging.debug("No date specified, using system date: %s",
+                      date.strftime(_date_format))
     else:
         try:
             date = datetime.datetime.strptime(args.date, _date_format)
@@ -41,25 +45,30 @@ if __name__ =='__main__':
     end_date = datetime.datetime.strptime(settings.end_date, _date_format)
 
     if date < start_date or date > end_date:
-        parser.exit(1, "Date (%s) is out of the %s..%s range.\n Check dates in settings.py\n"%(
-                date.strftime(_date_format), settings.start_date, settings.end_date))
+        parser.exit(1, "Date {0} is out of the {1}..{2} range.\n "
+                       " Check dates in settings.py\n".format(
+                            date.strftime(_date_format), settings.start_date,
+                            settings.end_date)
+                    )
 
-    #create report
+    # create report
     report_builder = report.Toggl(settings.api_token)
     workspaces = report_builder.get_workspaces()
 
     weeks = report.week_list(start_date, end_date)
     week_names = [m.strftime(settings.report_date_format) for (m, s) in weeks]
 
-    err_writer = csv.DictWriter(sys.stderr, ['user', 'team', 'duration', 'project', 'date', 'rule'])
+    err_writer = csv.DictWriter(sys.stderr,
+                                ['user', 'team', 'duration', 'project', 'date',
+                                 'rule'])
     err_writer.writeheader()
-    last_records = {} # last_records[user] = last_record
+    last_records = {}  # last_records[user] = last_record
 
     # super_report[team][user][course][week]
     super_report = defaultdict(
-                    lambda: defaultdict(
-                        lambda: defaultdict(
-                            lambda: defaultdict(lambda: 0))))
+        lambda: defaultdict(
+            lambda: defaultdict(
+                lambda: defaultdict(lambda: 0))))
 
     week_names = []
 
@@ -72,72 +81,82 @@ if __name__ =='__main__':
         for workspace in workspaces:
             records_read = 0
             page = 1
+            wsname = workspace['name']
             while True:
-                report = report_builder.detailed_report(workspace['id'], monday, sunday, page)
+                report = report_builder.detailed_report(workspace['id'], monday,
+                                                        sunday, page)
 
                 for record in report['data']:
-                    #record duration is in miliseconds, divide by 3600000 to convert to hours
-                    hours = round(float(record['dur'])/3600000, 2)
+                    # record duration is in milliseconds
+                    # divide by 3600000 to convert to hours
+                    hours = round(float(record['dur']) / 3600000, 2)
                     start = record['start'].split('T', 1)[0]
+                    user = record['user']
+                    project = record['project']
 
-                    # TIME LOGGING SANITY CHECK - long records, missing project, overlapping
+                    # TIME LOGGING SANITY CHECK
+                    # long records, missing project, overlapping
 
                     # missing project
-                    if not record['project']: # usually None, but want to detect empty string as well
-                        record['project'] = '(no project)'
+                    # usually None, but want to detect empty string as well
+                    if not project:
+                        record['project'] = project = '(no project)'
                         err_writer.writerow({
-                            'user'    : record['user'],
-                            'team'    : workspace['name'],
-                            'rule'    : 'record without project',
+                            'user': user,
+                            'team': wsname,
+                            'rule': 'record without project',
                             'duration': hours,
-                            'project' : record['project'],
-                            'date'    : start,
-                            })
+                            'project': project,
+                            'date': start,
+                        })
 
                     # check for overlapping entry
-                    if record['user'] in last_records and last_records[record['user']]['end'] > record['start']:
+                    if user in last_records and \
+                            last_records[user]['end'] > record['start']:
                         err_writer.writerow({
-                            'user'    : record['user'],
-                            'team'    : workspace['name'],
-                            'rule'    : 'overlapping entry: %(start)s %(project)s'%record,
+                            'user': user,
+                            'team': wsname,
+                            'rule': 'overlapping entry: %(start)s %(project)s' % record,
                             'duration': hours,
-                            'project' : record['project'],
-                            'date'    : start,
-                            })
-                    last_records[record['user']] = record
+                            'project': project,
+                            'date': start,
+                        })
+                    last_records[user] = record
 
                     # long records
                     if hours > args.threshold:
                         err_writer.writerow({
-                            'user'    : record['user'],
-                            'team'    : workspace['name'],
-                            'rule'    : 'record > %s hours'% args.threshold,
+                            'user': user,
+                            'team': wsname,
+                            'rule': 'record > %s hours' % args.threshold,
                             'duration': hours,
-                            'project' : record['project'],
-                            'date'    : start,
-                            })
+                            'project': project,
+                            'date': start,
+                        })
 
                     # ACTUAL REPORTING
-                    super_report[workspace['name']][record['user']][record['project']][week] = \
-                        round(super_report[workspace['name']][record['user']][record['project']][week] + hours, 2)
+                    super_report[wsname][user][project][week] = \
+                        round(super_report[wsname][user][project][week]+hours, 2)
 
                 records_read += report['per_page']
                 page += 1
                 if records_read >= report['total_count']:
                     break
 
-    report_writer = csv.DictWriter(sys.stdout, ['user', 'team', 'project', 'avg'] + week_names)
+    report_writer = csv.DictWriter(sys.stdout, ['user', 'team', 'project',
+                                                'avg'] + week_names)
     report_writer.writeheader()
 
     for team, team_records in super_report.items():
-        for user, user_records in team_records.items(): # user_records = super_report[team][user]
-            for project, project_records in user_records.items(): # project_records = super_report[team][user][project]
+        for user, user_records in team_records.items():
+            # user_records = super_report[team][user]
+            for project, project_records in user_records.items():
+                # project_records = super_report[team][user][project]
                 average = sum(project_records.values()) / len(project_records)
                 project_records.update({
-                    'user' : user,
-                    'team' : team,
+                    'user': user,
+                    'team': team,
                     'project': project,
-                    'avg'  : round(average, 2),
+                    'avg': round(average, 2),
                 })
                 report_writer.writerow(project_records)
-
--- report.py
+++ report.py
@@ -15,9 +15,10 @@ import datetime
 # Note it might be different from Toggl.date_format
 _date_format = "%Y-%m-%d"
 
+
 class Toggl(object):
     baseURL = 'https://toggl.com/'
-    date_format = '%Y-%m-%d' #YYYY-MM-DD
+    date_format = '%Y-%m-%d'  # YYYY-MM-DD
 
     def __init__(self, api_token):
         self.logger = logging.getLogger(__name__)
@@ -25,32 +26,34 @@ class Toggl(object):
         # HTTP Auth magic
         # TODO: replace with urllib2 opener
         chunks = self.baseURL.split("//", 1)
-        if len(chunks) < 2: #no https? in url
+        if len(chunks) < 2:  # no https? in url
             chunks[:0] = 'https:',
-        self.baseURL = ''.join((chunks[0], "//", ":".join((self.api_token, 'api_token@')), chunks[1]))
+        self.baseURL = ''.join((chunks[0], "//",
+                                ":".join((self.api_token, 'api_token@')),
+                                chunks[1]))
 
     def _build_url(self, api_func, params):
         # build ULR of API request, which looks like:
         # https://${base_url}/{api_func}?param1=foo&param2=bar
         query = '' if params is None else '?' + urllib.urlencode(params)
         url = ''.join((self.baseURL, api_func, query))
-        self.logger.debug("_build_url: %s:"%url)
+        self.logger.debug("_build_url: %s:" % url)
         return url
 
     def _get_json(self, url):
         try:
             response = urllib.urlopen(url)
         except IOError:
-            self.logger.error('Failed to open url: %s'%url)
+            self.logger.error('Failed to open url: %s' % url)
             raise
         response_text = response.read()
-        self.logger.debug("Response from Toggl: %s"%response_text)
+        self.logger.debug("Response from Toggl: %s" % response_text)
         response_json = json.loads(response_text)
         if 'error' in response_json:
             self.logger.error("""Error getting Toggl data:
             message: %(message)s
             tip: %(tip)s
-            code: %(code)s"""%response_json['error'])
+            code: %(code)s""" % response_json['error'])
             raise
         return response_json
 
@@ -59,36 +62,36 @@ class Toggl(object):
         return self._get_json(url)
 
     def get_workspaces(self):
-        #get workspaces list
+        # get workspaces list
         data = self._request('api/v8/workspaces')
 
-        #filter out personal workspaces:
+        # filter out personal workspaces:
         workspaces = [{'name': w['name'], 'id': w['id']} for w in data \
-                            if 'personal' not in w['name'] and w['admin']]
+                      if 'personal' not in w['name'] and w['admin']]
 
-        #get number of people in workspace
+        # get number of people in workspace
         for workspace in workspaces:
-            users = self._request('api/v8/workspaces/%s/users'%workspace['id'])
-            workspace['members'] = sum([u['email'] not in settings.admin_emails for u in users])
+            users = self._request(
+                'api/v8/workspaces/%s/users' % workspace['id'])
+            workspace['members'] = sum(
+                [u['email'] not in settings.admin_emails for u in users])
 
         return workspaces
 
     def get_projects(self, workspace_id):
-        data = self._request('api/v8/workspaces/%s/projects'%workspace_id)
+        data = self._request('api/v8/workspaces/%s/projects' % workspace_id)
         return data
 
     def weekly_report(self, workspace_id, since, until):
-        """
-        Toggl weekly report for a given team
-        """
-        #TODO: to support students failing the program, check number of team members for every week
+        """ Toggl weekly report for a given team """
         return self._request('reports/api/v2/weekly', {
-            'workspace_id'  : workspace_id,
-            'since'         : since.strftime(self.date_format),
-            'until'         : until.strftime(self.date_format),
-            'user_agent' : 'github.com/user2589/Toggl.py',
-            'order_field': 'title', #title/day1/day2/day3/day4/day5/day6/day7/week_total in weekly report
-            'display_hours': 'decimal', #decimal/minutes
+            'workspace_id': workspace_id,
+            'since': since.strftime(self.date_format),
+            'until': until.strftime(self.date_format),
+            'user_agent': 'github.com/user2589/Toggl.py',
+            'order_field': 'title',
+            # title/day1/day2/day3/day4/day5/day6/day7/week_total
+            'display_hours': 'decimal',  # decimal/minutes
         })
 
     def detailed_report(self, workspace_id, since, until, page=1):
@@ -96,23 +99,24 @@ class Toggl(object):
         Toggl detailed report for a given team
         """
         return self._request('reports/api/v2/details', {
-            'workspace_id'  : workspace_id,
-            'since'         : since.strftime(self.date_format),
-            'until'         : until.strftime(self.date_format),
-            'user_agent'    : 'github.com/user2589/Toggl.py',
-            'order_field'   : 'date',    #date/description/duration/user in detailed reports
-            'order_desc'    : 'off', #on/off
-            'display_hours' : 'decimal', #decimal/minutes
-            'page'          : page
+            'workspace_id': workspace_id,
+            'since': since.strftime(self.date_format),
+            'until': until.strftime(self.date_format),
+            'user_agent': 'github.com/user2589/Toggl.py',
+            'order_field': 'date',
+            # date/description/duration/user in detailed reports
+            'order_desc': 'off',  # on/off
+            'display_hours': 'decimal',  # decimal/minutes
+            'page': page
         })
 
 
 def last_sunday(date):
-    return date - datetime.timedelta(days=date.weekday()+1)
+    return date - datetime.timedelta(days=date.weekday() + 1)
+
 
 def week_list(start_date, end_date):
-    """
-    returns list of datetime tuples (monday, sunday) for every week in requested interval
+    """ List of datetime tuples (monday, sunday) for every week in the interval
     """
     weeks = []
     sunday = last_sunday(start_date)
@@ -122,25 +126,29 @@ def week_list(start_date, end_date):
         sunday += datetime.timedelta(days=7)
     return weeks
 
+
 if __name__ == '__main__':
-    #parse parameters
-    parser = argparse.ArgumentParser(description='Create weekly time report for CMU SE programm for the past week.'
-            ' HTML printed to stdout')
+    # parse parameters
+    parser = argparse.ArgumentParser(
+        description="Create weekly time report for CMU SE programm for the "
+                    "past week. HTML printed to stdout")
     parser.add_argument('-d', '--date', help='system date override, YYYY-MM-DD')
-    parser.add_argument('-v', '--verbose', help="Verbose mode, 5 - extra verbose, "
-                        "1 - mute, default: 3", default=3)
+    parser.add_argument('-v', '--verbose',
+                        help="Verbose mode, 5 - extra verbose, "
+                             "1 - mute, default: 3", default=3)
     args = parser.parse_args()
 
     # configure verboseness
     try:
-        verboseness = max(1, 5 - int(args.verbose)*1) * 10
+        verboseness = max(1, 5 - int(args.verbose) * 1) * 10
     except ValueError:
         verboseness = 30
     logging.basicConfig(level=verboseness)
 
     if args.date is None:
         date = datetime.datetime.now()
-        logging.debug("No date specified, using system date: %s", date.strftime(_date_format))
+        logging.debug("No date specified, using system date: %s",
+                      date.strftime(_date_format))
     else:
         try:
             date = datetime.datetime.strptime(args.date, _date_format)
@@ -151,16 +159,19 @@ if __name__ == '__main__':
     end_date = datetime.datetime.strptime(settings.end_date, _date_format)
 
     if date < start_date or date > end_date:
-        parser.exit(1, "Date (%s) is out of the %s..%s range.\n Check dates in settings.py\n"%(
-                date.strftime(_date_format), settings.start_date, settings.end_date))
+        parser.exit(1, "Date {0} is out of the {1}..{2} range.\n Check dates in"
+                       " settings.py\n".format(
+                           date.strftime(_date_format), settings.start_date,
+                           settings.end_date))
 
-    #create report
+    # create report
     report_builder = Toggl(settings.api_token)
     workspaces = report_builder.get_workspaces()
 
     logging.debug("Raw workspaces JSON:\n %s", json.dumps(workspaces))
 
-    # super_report has 3 dimensions: weeks, course (aka project), and team (aka workspace), i.e.:
+    # super_report has 3 dimensions: weeks, course (aka project), and
+    # team (aka workspace), i.e.:
     # for week in weeks:
     #    for course in courses:
     #        for team in teams:
@@ -171,10 +182,12 @@ if __name__ == '__main__':
 
     weeks = week_list(start_date, end_date)
     week_names = [m.strftime(settings.report_date_format) for (m, s) in weeks]
-    # courses not in settings.core counted as meta project settings.everething_else
+    # courses not in settings.core counted as meta project, named after value
+    # of settings.everything_else
     electives = settings.everething_else
 
-    # for each team request reports up to 'date', build list of project and aggregate stats
+    # for each team request reports up to 'date', build list of project and
+    # aggregate stats
 
     for (monday, sunday) in weeks:
 
@@ -187,12 +200,15 @@ if __name__ == '__main__':
             team = workspace['name']
             super_report[week][team] = {}
             super_report[week][team][electives] = 0
-            report = report_builder.weekly_report(workspace['id'], monday, sunday)
+            report = report_builder.weekly_report(workspace['id'], monday,
+                                                  sunday)
 
             for project in report['data']:
 
-                #project['totals'] is in miliseconds, divide by 3600000 to convert to hours
-                hours = float(project['totals'][-1])/(3600000*workspace['members'])
+                # project['totals'] is in miliseconds
+                # divide by 3600000 to convert to hours
+                hours = float(project['totals'][-1]) / (
+                    3600000 * workspace['members'])
 
                 course = project['title']['project']
                 if course in settings.core_courses:
@@ -202,8 +218,9 @@ if __name__ == '__main__':
 
     logging.debug("Raw super_report JSON:\n %s", json.dumps(super_report))
 
-    # super_report contains all the data we need, but it is not suitable for rendering.
-    # here it will be converted to report_data, which will be iterated this way:
+    # super_report contains all the data we need, but it is not suitable for
+    # rendering. here it will be converted to report_data, which will be
+    # iterated this way:
     # report_data[course][team] = [week1, week2, week3...week12]
 
     team_names = [ws['name'] for ws in workspaces]
@@ -219,21 +236,24 @@ if __name__ == '__main__':
                     report_data[course] = {}
                 report_data[course][team] = []
                 for week in week_names:
-                    if week in super_report and team in super_report[week] and course in super_report[week][team]:
-                        report_data[course][team].append(round(super_report[week][team][course], 2))
+                    if week in super_report and team in super_report[week] and \
+                            course in super_report[week][team]:
+                        report_data[course][team].append(
+                            round(super_report[week][team][course], 2))
                     else:
                         report_data[course][team].append(None)
 
     logging.debug("Raw report_data JSON:\n %s", json.dumps(report_data))
 
-    course_names = [c for c in settings.core_courses+[electives] if c in report_data]
+    course_names = [c for c in settings.core_courses + [electives] if
+                    c in report_data]
 
-    #generate report file
+    # generate report file
     template_vars = {
-        'courses'       : json.dumps(course_names),
-        'week_labels'   : json.dumps(week_names),
-        'report_data'   : json.dumps(report_data),
-        'teams'         : json.dumps(team_names)
+        'courses': json.dumps(course_names),
+        'week_labels': json.dumps(week_names),
+        'report_data': json.dumps(report_data),
+        'teams': json.dumps(team_names)
     }
 
-    print settings.template%template_vars
+    print settings.template % template_vars
